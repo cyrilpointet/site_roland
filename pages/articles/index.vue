0c646f85b20f014512c5a6c1083f665d9ce92192
CyrilPurjus <c.pointet@purjus.fr>
2020-06-08 00:59:04 +0200

    inital commit

---
+++ pages/articles/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>blogPost</h1>
+  <div>
+    <p v-for="(blogPost, index) in blogPosts" :key="index">
+      {{ blogPost.title }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    blogPosts() {
+      return this.$store.state.blogPosts
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div>
+      <h1>
+        site_roland
+      </h1>
+      <h2>
+        site vitrine + blog
+      </h2>
+      <span>
+        <nuxt-link to="/">Accueil</nuxt-link>
+      </span>
+      <span>
+        <nuxt-link to="/articles">blogPost</nuxt-link>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      script: [
+        { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }
+      ]
+    }
+  }
+}
+</script>
