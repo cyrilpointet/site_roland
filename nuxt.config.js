172668b7fd893fc52973755e784a3b9bb552614c
CyrilPurjus <c.pointet@purjus.fr>
2020-06-08 02:44:32 +0200

    renommage router

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
       const path = require('path')
       return fs.readdirSync('./assets/content/blog').map((file) => {
         return {
-          route: `/blog/${path.parse(file).name}`, // Return the slug
+          route: `/articles/${path.parse(file).name}`, // Return the slug
           payload: require(`./assets/content/blog/${file}`)
         }
       })
