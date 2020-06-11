4c01072fb46437a30c67162f11977c0fdfeeacaf
CyrilPurjus <c.pointet@purjus.fr>
2020-06-11 17:47:14 +0200

    post request change url again

--- components/contact/contactForm.vue
+++ components/contact/contactForm.vue
@@ -133,7 +133,7 @@ export default {
           phone: this.phone,
           message: this.message
         }
-        fetch('/contact', {
+        fetch('/', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
